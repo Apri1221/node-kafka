0290227635fb0668fc07b19d4be0305754a8736a
Apriyanto Tobing <apriyanto.tobing@bni.co.id>
2023-09-04 16:32:09 +0700

    feat: proto nodejs

--- readme.md
+++ readme.md
@@ -20,3 +20,15 @@ curl --location 'http://localhost:3000/test?topic=test' \
     "data": "apri"
 }'
 ```
+
+[POST] /test?topic=(required)&proto=(required)
+```
+curl --location 'http://localhost:3000/test/proto?topic=streaming.transaction.singleEvent&proto=streaming.transaction.singleEvent-id.co.bni.maverick.proto.kafka.TransferBiFast' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: token=60ab7270009bb239be67e50c87971bec71dccbc72dc49cd96865db29c233b3f4' \
+--data '{
+    "transactionId": "1231413",
+    "expiryTime": "2023-12-12",
+    "scenario": "scenario"
+}'
+```
--- src/index.js
+++ src/index.js
@@ -2,26 +2,67 @@
 import app from './app.js';
 import { Kafka, logLevel } from "kafkajs";
 
+// https://kafkajs.github.io/confluent-schema-registry/docs/usage
+import { SchemaRegistry, SchemaType } from '@kafkajs/confluent-schema-registry'
+
+const registry = new SchemaRegistry({ 
+    host: 'http://192.168.244.109:8081',
+    retry: {
+        maxRetryTimeInSecs: 5,
+        initialRetryTimeInSecs: 0.1,
+        factor: 0.2,
+        multiplier: 2,
+        retries: 3,
+      },
+ })
+
 
 const clientId = "node-js-svc"
 const brokers = ["192.168.244.109:9092"]
 const defaultTopic = "streaming.transaction.singleEvent"
+const defaultSubject = "streaming.transaction.singleEvent-id.co.bni.maverick.proto.kafka.TransferBiFast"
 
 const kafka = new Kafka({ 
     clientId, 
     brokers,
-    logLevel: logLevel.DEBUG, 
+    // logLevel: logLevel.DEBUG, 
 })
 
 const producer = kafka.producer()
 
+const proto = async (value) => {
+    try {
+        console.log("--- construct PROTO ----")
+        const myObject = {
+            transactionId: String(Math.floor(Math.random() * 10000) + 1),
+            expiryTime: String(Date.now()),
+            scenario: String("scenario")
+        }
+        if (value) {
+            myObject = JSON.parse(value);
+        }
+        
+        console.log(myObject);
+        const id = await registry.getLatestSchemaId(defaultSubject)
+        console.log("ID subject ", id)
+        const record = await registry.encode(id, myObject);
+        return record;
+    }
+    catch (err) {
+        console.error("could not write proto " + err);
+    }
+}
 
-const produce = async (value, customTopic = null) => {
+const produce = async (value, customTopic = null, customProto = null) => {
 	await producer.connect()
+
+    if (customProto) {
+        value = await proto(value);
+        console.log("proto decode ", await registry.decode(value))
+    }
     let topic = customTopic === null ? defaultTopic : customTopic;
     try {
         let id = String(Math.floor(Math.random() * 10000) + 1);
-        
         await producer.send({
             topic,
             messages: [
@@ -53,6 +94,18 @@ app.post('/test', (req, res, next) => {
     let topic = req.query.topic;
     
     produce(JSON.stringify(val), topic).catch((err) => {
+        console.error("error in producer: ", err)
+    })
+    return res.status(200).send(val);
+})
+
+
+app.post('/test/proto', (req, res, next) => {
+    let val = req.body;
+    let topic = req.query.topic;
+    let subject = req.query.proto;
+    
+    produce(JSON.stringify(val), topic, subject).catch((err) => {
         console.error("error in producer: ", err)
     })
     return res.status(200).send(val);
